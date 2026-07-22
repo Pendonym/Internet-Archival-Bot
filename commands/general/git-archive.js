0e27c5c63a5261b3bafce096d3559e608ad1040c
Pendonym <265675176+Pendonym@users.noreply.github.com>
2026-07-22 17:28:35 -0500

    removing junk

--- commands/general/git-archive.js
+++ commands/general/git-archive.js
@@ -1,97 +1,26 @@
-// apparently these are the issues for the crash (thank you vad for lmk but i have zero clue how to implement)
-//- git logs sometimes hit 1mb+, pass { maxBuffer: 1024 * 1024 * 50 } with execFile
-//- await and trycatch uploadArchive cause if any functions fail inside it, the process gets termed
-//- you should listen to stderr and use the trycatch's catch() part. you can write catch(error) and then inside the catch you can print the error to stdout
-
-// maybe juist switch to software heritiage faster, easier, etc
+// todo: use software heritiage api
 const { SlashCommandBuilder } = require('discord.js');
 const { exec, execFile } = require("child_process");
 const { glob } = require('glob')
+const http = require('node:http')
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('git-archive')
         .setDescription('Downloads Git repos videos and uploads them to archive.org')
-        .addStringOption((option) => option.setName('link').setDescription('The URL to the git repo.').setRequired(true))
-        .addBooleanOption((option) =>
-		    option.setName('include-wiki').setDescription('Clone and archive the repository wiki'),
-	    )
-        .addBooleanOption((option) =>
-		    option.setName('all-releases').setDescription('Download all releases'),
-	    )
-        .addBooleanOption((option) =>
-		    option.setName('all-branches').setDescription('Clone every branch in the repository'),
-	    ),
+        .addStringOption((option) => option.setName('link').setDescription('The URL to the git repo.').setRequired(true)),
 
     async execute(interaction) {
         const url = interaction.options.getString('link');
-        const includewiki = interaction.options.getBoolean('include-wiki');
-        const allreleases = interaction.options.getBoolean('all-releases');
-        const allbranches = interaction.options.getBoolean('all-branches');
-
-        console.log(allbranches, includewiki, allreleases)
 
         if (!/^https?:\/\//i.test(url)) {
             return interaction.reply('Please provide a valid URL.');
         }
 
-        const sent = await interaction.reply({ content: 'Sending request...', withResponse: true });
+        await interaction.reply({ content: 'Sending request...', withResponse: true });
 
-        function uploadArchive() {
-            ///lily
-			try
-			{
-				///pendonym
-				const command = [ url ];
-
-	            if (includewiki) {
-	                console.log(`include wiki yes`)
-	                command.push("--include-wiki")
-	            }
-	            if (allreleases) {
-	                console.log(`include release yes`)
-	                command.push("--include-wiki")
-	            }
-	            if (allbranches) {
-	                console.log(`include branch yes`)
-	                command.push("--include-wiki")
-	            }
-	
-	            return new Promise((resolve, reject) => {
-                    console.log(command)
-	                execFile('iagitbetter', command, (error, stdout, stderr) => {
-	                    if (error) {
-                            interaction.editReply(`Archived prolly error`);
-	                        console.log(`node error: ${error.message}`);
-	                        return reject(error);
-	                    }
-	                    if (stderr) {
-                            interaction.editReply(`Archived prolly stderr`);
-	                        console.log(`error: ${stderr}`)
-	                        return reject(stderr)
-	                    }
-	                    if (stdout) {
-                            interaction.editReply(`Archived prolly stdout`);
-                            console.log(stdout);
-                            resolve(stdout);
-                        }
-                        interaction.editReply(`Archived prolly none of those 3`);
-	                });
-	            });
-			}
-
-			catch (err)
-			{
-				console.log("bad stuf:", err);
-			}
-        }
-
-        uploadArchive()
-			.then((value) => {
-			    console.log(value);
-			})
-			.catch((err) => {
-			    console.error("bad stuf (part 2):",err);
-			});
+        function sendRequest() {
+			
+		}
     },
 };
